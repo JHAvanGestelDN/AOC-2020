ac9a68c837256c09df798620cdc8f1ecafb023a9
Jaap van Gestel <jaapvg@delta-n.nl>
2020-12-03 07:31:51 +0100

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sample =
+                System.IO.File.ReadAllLines(@"C:\Users\admin\RiderProjects\AdventOfCode\Assets\3.1.txt");
+
+            int[] rowIncrease = {1, 1, 1, 1, 2};
+            int[] colIncrease = {1, 3, 5, 7, 1};
+
+            int result = 1;
+            for (int i = 0; i < rowIncrease.Length; i++)
+            {
+                result *= CleaverMethodName(rowIncrease[i], colIncrease[i], sample);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int CleaverMethodName(int rowIncrease, int colIncrease, string[] input)
+        {
+            int trees = 0;
+            int j = colIncrease;
+            for (int i = rowIncrease; i < input.Length; i += rowIncrease)
+            {
+                if (j >= input[i].Length)
+                    j = 0 + (j - input[i].Length);
+
+                if (input[i][j] == '#')
+                {
+                    trees++;
+                }
+
+                j += colIncrease;
+            }
+
+            return trees;
+        }
+    }
+}
